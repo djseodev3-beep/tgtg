cdeea2c1648c6b9e07f3aef3db76f88b6869f101
djseo_Work <dj.seo.dev3@gmail.com>
2025-08-19 17:14:00 +0900

    feat(domain) : Review Entity 데이터 추가, product columnDefinition= "TEXT" 테스트 시 오류 발생. 원인 파악 필요

--- src/main/java/com/spring/tgtg/review/domain/Review.java
+++ src/main/java/com/spring/tgtg/review/domain/Review.java
@@ -2,6 +2,8 @@ package com.spring.tgtg.review.domain;
 
 import com.spring.tgtg.common.jpa.BaseTimeEntity;
 import com.spring.tgtg.reservation.domain.Reservation;
+import com.spring.tgtg.store.domain.Store;
+import com.spring.tgtg.user.domain.User;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -21,4 +23,15 @@ public class Review extends BaseTimeEntity {
     @JoinColumn(name = "reservation_id", nullable = false)
     private Reservation reservation;
 
+    @ManyToOne(fetch = FetchType.LAZY) @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY) @JoinColumn(name = "store_id", nullable = false)
+    private Store store;
+
+    @Column(nullable = false)
+    private int rating; // 1~5
+
+    @Column(nullable = false, length = 500)
+    private String content;
 }
