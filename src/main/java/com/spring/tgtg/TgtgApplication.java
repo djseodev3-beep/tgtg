19b7859c481fd1916fa9a22928127e29084cada2
djseo_Work <dj.seo.dev3@gmail.com>
2025-08-19 15:42:04 +0900

    chore(repo): add untracked files
    - Added gradle wrapper and scripts
    - Included example configs and docs
    - Ensured repository is consistent after .gitignore update

---
+++ src/main/java/com/spring/tgtg/TgtgApplication.java
@@ -0,0 +1,15 @@
+package com.spring.tgtg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class TgtgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TgtgApplication.class, args);
+    }
+
+}
